b3f8f8fae9bffac4fd6302d2ab32b67bdaab24d7
Abhigyan Madhukalya <amadhukalya2005@gmail.com>
2024-03-27 13:45:01 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Abhigyan Madhukalya's Nix flake";
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/default/hardware-configuration.nix
+          ./hosts/default/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.abhigyan = import ./hosts/default/home.nix;
+	  }
+	  {
+	    nix.settings.trusted-users = [ "abhigyan" ];
+	  }
+        ];
+      };
+    };
+  };
+}
---
+++ hosts/default/home.nix
@@ -0,0 +1,95 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "abhigyan";
+  home.homeDirectory = "/home/abhigyan";
+
+  home.packages = with pkgs; [
+    neofetch
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep
+    jq
+    yq-go
+    eza
+    fzf
+
+    # networking tools
+    mtr
+    aria2
+    nmap
+    dnsutils
+
+    # misc
+    file
+    which
+    tree
+    gnutar
+    zstd
+    gnupg
+    gnused
+
+    # nix-reladted
+    nix-output-monitor
+
+    # sys tools
+    lm_sensors
+    pciutils
+    usbutils
+    duf
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Abhigyan Madhukalya";
+    userEmail = "amadhukalya2005@gmail.com";
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+	draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+    shellAliases = {
+      vim = "nvim";
+      btop = "btop --utf-force";
+      ls = "eza --long --header --git --all --icons=always";
+      ll = "eza -l -h --icons=always";
+    };
+  };
+
+
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+}
