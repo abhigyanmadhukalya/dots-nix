94b0cbe7b8a9ac71256734076ee285dea570ddc3
Abhigyan Madhukalya <amadhukalya2005@gmail.com>
2024-03-31 14:18:06 +0530

    virtual box

--- hosts/default/default.nix
+++ hosts/default/default.nix
@@ -27,6 +27,8 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.auto-optimise-store = true;
 
+  virtualisation.virtualbox.host.enable = true; # Virtualisation
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
